57896f849a18aa342f4893a6317d5e62f22a9710
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-04 08:01:44 -0500

    formatiados

--- 06-React-Intro/homework/src/App.js
+++ 06-React-Intro/homework/src/App.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import './App.css';
-import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import data, { Cairns as cairns, Londres, Denver } from './data.js';
+// import React from 'react';
+import "./App.css";
+// import Card from "./components/Card.jsx";
+import Cards from "./components/Cards.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import data /* { Cairns } */ from "./data.js";
 
 function App() {
   // console.log(ciudad);
   return (
     <div className="App">
+      {/* <div>
+        <Card
+          max={Cairns.main.temp_max}
+          min={Cairns.main.temp_min}
+          name={Cairns.name}
+          img={Cairns.weather[0].icon}
+          onClose={() => alert(Cairns.name)}
+        />
+      </div> */}
+      <hr />
       <div>
-        {/* <Card
-          max={cairns.main.temp_max}
-          min={cairns.main.temp_min}
-          name={cairns.name}
-          img={cairns.weather[0].icon}
-          onClose={() => alert(cairns.name)}
-        /> */}
+        <Cards cities={data} />
       </div>
       <hr />
       <div>
-        <Cards
-          cities={data}
-        />
-      </div>
-      <hr />
-      <div>
-        <SearchBar
-          onSearch={(ciudad) => alert(ciudad)}
-        />
+        <SearchBar onSearch={(ciudad) => alert(ciudad)} />
       </div>
     </div>
   );
