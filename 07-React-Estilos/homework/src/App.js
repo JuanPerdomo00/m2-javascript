163a7d25e1ba223a9dde110f3c791dfc27981704
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-04 16:35:20 -0500

    Se modifico app

--- 07-React-Estilos/homework/src/App.js
+++ 07-React-Estilos/homework/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import Card from './components/Card.jsx';
+// import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
-import data, { Cairns } from './data.js';
+import data/* , { Cairns } */ from './data.js';
 
 function App() {
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <Card
           max={Cairns.main.temp_max}
           min={Cairns.main.temp_min}
@@ -16,7 +16,7 @@ function App() {
           img={Cairns.weather[0].icon}
           onClose={() => alert(Cairns.name)}
         />
-      </div>
+      </div> */}
       <hr />
       <div>
         <Cards
